4e47d837fe70d482e0b1a5cf68b5d3ed3a6f22c5
Jactus <dw1920@nyu.edu>
2019-05-24 14:45:04 +0800

    Fix edge smoothing bug

--- smooth_edge.py
+++ smooth_edge.py
@@ -16,13 +16,13 @@ def smooth_edge(path, img_size=256):
         file_name = os.path.basename(f)
 
         # deal with rgb images
-        rgb_img = cv2.imread(f)
-        rgb_img = cv2.resize(rgb_img, (img_size, img_size))
+        rgb_img = cv2.imread(path+'/'+f)
+        rgb_img = cv2.resize(rgb_img, (img_size, img_size), interpolation=cv2.INTER_CUBIC)
         pad_img = np.pad(rgb_img, ((2, 2), (2, 2), (0, 0)), mode='reflect')
 
         # deal with gray images
-        gray_img = cv2.imread(f, 0)
-        gray_img = cv2.resize(gray_img, (img_size, img_size))
+        gray_img = cv2.imread(path+'/'+f, 0)
+        gray_img = cv2.resize(gray_img, (img_size, img_size), interpolation=cv2.INTER_CUBIC)
 
         # get the edges and dilations
         edges = cv2.Canny(gray_img, 100, 200)
@@ -36,7 +36,7 @@ def smooth_edge(path, img_size=256):
             result[idx[0][i], idx[1][i], 1] = np.sum(np.multiply(pad_img[idx[0][i]:idx[0][i] + kernel_size, idx[1][i]:idx[1][i] + kernel_size, 1], gauss))
             result[idx[0][i], idx[1][i], 2] = np.sum(np.multiply(pad_img[idx[0][i]:idx[0][i] + kernel_size, idx[1][i]:idx[1][i] + kernel_size, 2], gauss))
         
-        cv2.imwrite(os.path.join(save_path, file_name), gauss_img)
+        cv2.imwrite(os.path.join(save_path, file_name), result)
 
 if __name__ == "__main__":
     smooth_edge('dataset/cartoon_imgs')
