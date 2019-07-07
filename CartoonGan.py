bfc7014a905f24cf2b93c80b2991397fa6b65df6
Dingsu Wang <dingsu.wang@gmail.com>
2019-07-07 17:21:22 +0800

    Update training script

--- CartoonGan.py
+++ CartoonGan.py
@@ -5,7 +5,9 @@ from tensorflow.python.keras.models import Model
 from tensorflow.python.keras.layers import *
 from tensorflow.python.keras.optimizers import *
 from tensorflow.python.keras.callbacks import TensorBoard
+from tensorflow.python.keras.utils import multi_gpu_model
 from utils import *
+from keras_contrib.layers import InstanceNormalization
 
 # class for CartoonGAN
 class CartoonGAN():
@@ -18,7 +20,7 @@ class CartoonGAN():
         self.log_dir = args.log_dir
         self.lr = args.lr
         self.weight = args.weight
-    
+
     # method for generator
     def generator(self):
         input_shape=[self.image_size, self.image_size, self.image_channels]
@@ -27,7 +29,7 @@ class CartoonGAN():
         # first block
         x = ZeroPadding2D(3)(input_img)
         x = Conv2D(64, (7, 7), strides=1, use_bias=True, padding='valid', name="conv1")(x)
-        x = BatchNormalization(name="norm1")(x)
+        x = InstanceNormalization(name="norm1")(x)
         x = Activation("relu")(x)
 
         # down-convolution
@@ -35,20 +37,20 @@ class CartoonGAN():
         for i in range(2):
             x = Conv2D(channel, (3, 3), strides=2, use_bias=True, padding='same', name="conv{}_1".format(i+2))(x)
             x = Conv2D(channel, (3, 3), strides=1, use_bias=True, padding='same', name="conv{}_2".format(i+2))(x)
-            x = BatchNormalization(name="norm{}".format(i+2))(x)
+            x = InstanceNormalization(name="norm{}".format(i+2))(x)
             x = Activation("relu")(x)
             channel = channel * 2
-        
+
         # residual blocks
         x_res = x
         for i in range(8):
             x = ZeroPadding2D(1)(x)
             x = Conv2D(256, (3, 3), strides=1, use_bias=True, padding='valid', name="conv{}_1".format(i+4))(x)
-            x = BatchNormalization(name="norm{}_1".format(i+4))(x)
+            x = InstanceNormalization(name="norm{}_1".format(i+4))(x)
             x = Activation("relu")(x)
             x = ZeroPadding2D(1)(x)
             x = Conv2D(256, (3, 3), strides=1, use_bias=True, padding='valid', name="conv{}_2".format(i+4))(x)
-            x = BatchNormalization(name="norm{}_2".format(i+4))(x)
+            x = InstanceNormalization(name="norm{}_2".format(i+4))(x)
             x = Add()([x, x_res])
             x_res = x
 
@@ -56,7 +58,7 @@ class CartoonGAN():
         for i in range(2):
             x = Conv2DTranspose(channel//2, 3, 2, padding="same", name="deconv{}_1".format(i+1))(x)
             x = Conv2D(channel//2, (3, 3), strides=1, use_bias=True, padding="same", name="deconv{}_2".format(i+1))(x)
-            x = BatchNormalization(name="norm_deconv"+str(i+1))(x)
+            x = InstanceNormalization(name="norm_deconv"+str(i+1))(x)
             x = Activation("relu")(x)
             channel = channel // 2
 
@@ -64,7 +66,7 @@ class CartoonGAN():
         x = ZeroPadding2D(3)(x)
         x = Conv2D(3, (7, 7), strides=1, use_bias=True, padding="valid", name="deconv3")(x)
         x = Activation("tanh")(x)
-        
+
         model = Model(input_img, x, name='Cartoon_Generator')
 
         return model
@@ -84,15 +86,15 @@ class CartoonGAN():
             x = Conv2D(channel, (3, 3), strides=2, use_bias=True, padding='same', name="conv{}_1".format(i+2))(x)
             x = LeakyReLU(alpha=0.2)(x)
             x = Conv2D(channel*2, (3, 3), strides=1, use_bias=True, padding='same', name="conv{}_2".format(i+2))(x)
-            x = BatchNormalization()(x)
+            x = InstanceNormalization()(x)
             x = LeakyReLU(alpha=0.2)(x)
             channel = channel * 2
 
         # last block
         x = Conv2D(256, (3, 3), strides=1, use_bias=True, padding='same', name="conv4")(x)
-        x = BatchNormalization()(x)
+        x = InstanceNormalization()(x)
         x = LeakyReLU(alpha=0.2)(x)
-        
+
         x = Conv2D(1, (3, 3), strides=1, use_bias=True, padding='same', activation='sigmoid', name="conv5")(x)
 
         model = Model(input_img, x, name='Cartoon_Discriminator')
@@ -112,19 +114,22 @@ class CartoonGAN():
         # get l1 loss for the content loss
         y_true = vgg(y_true)
         y_pred = vgg(y_pred)
-        content_loss = tf.reduce_mean(tf.abs(y_true - y_pred))
+        content_loss = tf.losses.absolute_difference(y_true, y_pred)
 
         return content_loss
 
     # compile each model
     def compile_model(self):
         # init summary writer for tensorboard
-        self.callback1 = TensorBoard(self.log_dir+'_discriminator')
-        self.callback2 = TensorBoard(self.log_dir+'_generator')
-        
+        self.callback1 = TensorBoard(self.log_dir+'/discriminator')
+        self.callback2 = TensorBoard(self.log_dir+'/generator')
+        self.callback3 = TensorBoard(self.log_dir+'/generated_images')
+        # init the log file writer
+        self.sess = K.get_session()
+        self.writer = tf.summary.FileWriter( './logs ', self.sess.graph)
         # model stuff
         input_shape=[self.image_size, self.image_size, self.image_channels]
-        adam1 = Adam(lr=self.lr*5)
+        adam1 = Adam(lr=self.lr)
         adam2 = Adam(lr=self.lr)
 
         # init
@@ -138,52 +143,102 @@ class CartoonGAN():
         # compile generator
         input_tensor = Input(shape=input_shape)
         generated_catroon_tensor = self.generator(input_tensor)
+        self.discriminator.trainable = False
         discriminator_output = self.discriminator(generated_catroon_tensor)
         self.train_generator = Model(input_tensor, outputs=[generated_catroon_tensor, discriminator_output])
         self.train_generator.compile(loss=[self.vgg_loss, 'binary_crossentropy'],
                                              loss_weights=[float(self.weight), 1.0],
                                              optimizer=adam2)
-        
+
         # set callback model
         self.callback1.set_model(self.discriminator)
         self.callback2.set_model(self.train_generator)
+        self.callback3.set_model(self.train_generator)
 
     # method for training process
-    def train(self, batch_generator):
+    def train(self):
         # these two tensors measure the output of generator and discriminator
-        real = np.ones((batch_generator.batch_size,) + (64, 64, 1))
-        fake = np.zeros((batch_generator.batch_size,) + (64 , 64, 1))
+        #real = np.ones((self.batch_size,) + (64, 64, 1))
+        #fake = np.zeros((self.batch_size,) + (64 , 64, 1))
 
         # start training
+        flip = False
+        variance = 1 / 127.5
         start_time = time.time()
-        for epoch in range(self.epochs):
-            print('Epoch {}'.format(epoch+1))
-            for idx, (photo, cartoon, smooth_cartoon) in enumerate(batch_generator):
-                # make soft and noise labels
-                real = real + random.uniform(-0.2, 0.2)
-                fake = fake + random.uniform(0, 0.2)
+        for epoch in range(1,self.epochs+1):
 
-                # train discriminator
-                generated_img = self.generator.predict(photo)
-                real_loss = self.discriminator.train_on_batch(cartoon, real)
-                smooth_loss = self.discriminator.train_on_batch(smooth_cartoon, fake)
-                fake_loss = self.discriminator.train_on_batch(generated_img, fake)
-                d_loss = (real_loss + smooth_loss + fake_loss) / 3
+            # create batch generator at each epoch
+            batch_generator = DataGenerator(image_size=self.image_size, batch_size=self.batch_size)
+            batch_end = len(batch_generator)
+            print('Epoch {}'.format(epoch))
+            # start training for each batch
+            for idx, (photo, cartoon, smooth_cartoon, index) in enumerate(batch_generator):
+                if index + 1 == batch_end:
+                    break
+                real = np.ones((self.batch_size,) + (64, 64, 1))
+                fake = np.zeros((self.batch_size,) + (64 , 64, 1))
+                if epoch < 50:
+                    g_loss = self.train_generator.train_on_batch(photo, [photo, real])
+                    generated_img = self.generator.predict(photo)
+                    print("Batch %d (initial training for generator), g_loss: %.5f, with time: %4.4f" % (idx, g_loss[2], time.time()-start_time))
+                    start_time = time.time()
+                    write_log(self.callback2, 'g_loss', g_loss[2], idx + (epoch+1)*len(batch_generator))
+                    if idx % 20 == 0:
+                        write_images(self.callback3, generated_img, 'generated_imgs', idx + (epoch+1)*len(batch_generator))
+                    if epoch % 20 == 0 and K.eval(self.train_generator.optimizer.lr) > 0.0001:
+                        K.set_value(self.train_generator.optimizer.lr, K.eval(self.train_generator.optimizer.lr)*0.99)
+                else:
+                    # check if it is the end of an epoch
+                    if index + 1 == batch_end:
+                        break
+                    if variance > 0.000005:
+                        variance = variance * 0.99999
+                        gaussian = np.random.normal(0, variance, (cartoon.shape[1],cartoon.shape[2]))
+                        cartoon[:, :, :, 0] = cartoon[:, :, :, 0] + gaussian
+                        cartoon[:, :, :, 1] = cartoon[:, :, :, 1] + gaussian
+                        cartoon[:, :, :, 2] = cartoon[:, :, :, 2] + gaussian
+                        gaussian = np.random.normal(0, variance, (cartoon.shape[1],cartoon.shape[2]))
+                        smooth_cartoon[:, :, :, 0] = smooth_cartoon[:, :, :, 0] + gaussian
+                        smooth_cartoon[:, :, :, 1] = smooth_cartoon[:, :, :, 1] + gaussian
+                        smooth_cartoon[:, :, :, 2] = smooth_cartoon[:, :, :, 2] + gaussian
+                    # generate cartoonized images
+                    generated_img = self.generator.predict(photo)
+                    if idx % 9 == 0 or np.random.uniform(0, 1) < 0.05:
+                        real = fake
+                        fake = fake + 1
+                        flip = True
+                    # train discriminator and adversarial loss
+                    real_loss = self.discriminator.train_on_batch(cartoon, real)
+                    smooth_loss = self.discriminator.train_on_batch(smooth_cartoon, fake)
+                    fake_loss = self.discriminator.train_on_batch(generated_img, fake)
+                    d_loss = (real_loss + smooth_loss + fake_loss) / 3
 
-                # train generator
-                g_loss = self.train_generator.train_on_batch(photo, [photo, real])
-                print("Batch %d, d_loss: %.5f, g_loss: %.5f, with time: %4.4f" % (idx, d_loss, g_loss, time.time()-start_time))
-                start_time = time.time()
+                    # train generator
+                    if flip:
+                        real = fake
+                        fake = fake - 1
+                        flip = False
+                    real = np.ones((self.batch_size,) + (64, 64, 1))
+                    fake = np.zeros((self.batch_size,) + (64 , 64, 1))
+                    g_loss = self.train_generator.train_on_batch(photo, [photo, real])
+                    print("Batch %d, d_loss: %.5f, g_loss: %.5f, with time: %4.4f" % (idx, d_loss, g_loss[2], time.time()-start_time))
+                    start_time = time.time()
 
-                # add losses to writer
-                write_log(self.callback1, 'd_loss', d_loss, idx + (epoch+1)*len(batch_generator))
-                write_log(self.callback2, 'g_loss', g_loss, idx + (epoch+1)*len(batch_generator))
+                    # add losses to writer
+                    write_log(self.callback1, 'd_loss', d_loss, idx + (epoch+1)*len(batch_generator))
+                    write_log(self.callback2, 'g_loss', g_loss[2], idx + (epoch+1)*len(batch_generator))
+                    if idx % 20 == 0:
+                        write_images(self.callback3, generated_img, 'generated_imgs', idx + (epoch+1)*len(batch_generator))
 
-                # change learning rate 
-                if epoch % 100 == 0 and K.eval(self.discriminator.optimizer.lr) > 0.0001:
-                    K.set_value(self.discriminator.optimizer.lr, K.eval(self.discriminator.optimizer.lr)*0.95)
-                if epoch % 100 == 0 and K.eval(self.train_generator.optimizer.lr) > 0.0001:
-                    K.set_value(self.train_generator.optimizer.lr, K.eval(self.train_generator.optimizer.lr)*0.95)
-        
+                    # change learning rate
+                    if epoch % 10 == 0 and K.eval(self.discriminator.optimizer.lr) > 0.0001:
+                        K.set_value(self.discriminator.optimizer.lr, K.eval(self.discriminator.optimizer.lr)*0.95)
+                    if epoch % 10 == 0 and K.eval(self.train_generator.optimizer.lr) > 0.0001:
+                        K.set_value(self.train_generator.optimizer.lr, K.eval(self.train_generator.optimizer.lr)*0.95)
+                if epoch % 50 == 0:
+                    self.generator.save_weights('CartoonGan_generator_epoch_{}.h5'.format(epoch))
+                    self.discriminator.save_weights('CartoonGan_discriminator_epoch_{}.h5'.format(epoch))
+                    self.train_generator.save_weights('CartoonGan_train_generator_epoch_{}.h5'.format(epoch))
+
         print('Done!')
         self.generator.save('CartoonGan_generator.h5')
